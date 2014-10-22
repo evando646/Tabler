4c3047f1a85bc0067d7729d77485dda1d7a518fb
bylander <bylander@cs.utsa.edu>
2014-10-22 09:49:10 -0400

    First commit

---
+++ team-awesome-project/src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Initial file for testing new repo
+ * @author Tom Bylander
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Good luck to Team Awesome!");
+	}
+}
