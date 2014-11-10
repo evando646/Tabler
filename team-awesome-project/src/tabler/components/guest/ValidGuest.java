f5971fb6a4f452e22e2bef0998cb4082be780d4c
Roberto Mexquitic <rmstobi@gmail.com>
2014-11-10 16:27:46 -0600

    GuestModel is updated validGuest is now working and should be finished

--- team-awesome-project/src/tabler/components/guest/ValidGuest.java
+++ team-awesome-project/src/tabler/components/guest/ValidGuest.java
@@ -1,9 +1,33 @@
-package tabler.components.guest;
+package Guest;
+
+/*
+ * This class will check if the name field and 
+ * contact number filed are valid
+ * This class should be used first before createing the accual gues
+ */
 
 public class ValidGuest {
 	
+	/*
+	 * Use this method to check if the contact and name fields are both valid
+	 * Both must be valid to continue on.
+	 */
+	public static boolean checkGuest(String name,String contact){
+		if(checkContact(contact)&&checkName(name)){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 	
-	private boolean checkContact(String contact){
+	/*
+	 * will filter the contact string to leave only digits
+	 * Ideal input 1234567890 (123) 456 7890 (450)-897-4578
+	 * Only "." "()" "-" are allowed anything else will return false
+	 * Must be Ten digit number 
+	 */
+	private static boolean checkContact(String contact){
 		if(contact.length()<10){
 			return false;
 		}
@@ -23,12 +47,50 @@ public class ValidGuest {
 		return true;
 	}
 	
-	private boolean checkNum(String line[]){
+	/*
+	 * this method will check that the char in the line[] are all 
+	 * numberic numbers and that their are only and only 10 of them
+	 */
+	private static boolean checkNum(String line[]){
+		int count=0;
+
+		for(int i=0;i<line.length;i++){
 		
+			for(int j=0;j<line[i].length();j++){
+				if(!isInteger(String.valueOf(line[i].charAt(j)))){
+					return false;
+				}
+				count++;
+			}
+		}
+		if(count!=10){//if thier are not exactly ten then false
+			return false;
+		}
+		
+		return true;
 	}
 	
-	private boolean checkName(String name){
-		if(name.length()>1){
+	/*
+	 * This will check if the char string is a number or not
+	 * if it is it will return true otherwise false
+	 */
+	private static boolean isInteger(String s) {
+	    try { 
+	        Integer.parseInt(s); 
+	    } catch(NumberFormatException e) { 
+	        return false; 
+	    }
+	    
+	    return true;
+	}
+	
+	
+	/*
+	 * this will check the name field 
+	 * as long as it is not empty it will be true
+	 */
+	private static boolean checkName(String name){
+		if(name.length()>0){
 			return true;
 		}
 		else{
