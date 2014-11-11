9c7c1fb272fe3ceb8e97aef0ecf2312b70f083a4
mr-augustine <pushbuttonrobotics@gmail.com>
2014-11-10 21:18:07 -0600

    Renamed the test file for the GuestModel. Made it compilable.

--- team-awesome-project/src/test/GuestTest.java
+++ team-awesome-project/src/test/GuestTest.java
@@ -1,17 +1,16 @@
 package test;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.lang.Integer;
-public class testMain {
+import tabler.components.guest.*;
 
-	@SuppressWarnings("deprecation")
+public class GuestTest {
+
 	public static void main(String[] args) {
 		
 		GuestModel testModel=new GuestModel("bob","No stuff and things",false,"(512)-123-8524");
 		testModel.setReservationTime(15, 3, 2016, 9, 26);
 		System.out.println(testModel.toString());
-		System.out.println(testModel.reservationTimeRet());
+		System.out.println(testModel.getReservationTime());
 		
 		
 		String a="abs";
