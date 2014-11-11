a8dd51a20c3f2b26f93df8951a1ce03b4a641009
mr-augustine <pushbuttonrobotics@gmail.com>
2014-11-11 17:59:51 -0600

    First commit of ServerQueueModel.java

---
+++ team-awesome-project/src/tabler/components/server/ServerQueueModel.java
@@ -0,0 +1,13 @@
+package tabler.components.server;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ServerQueueModel {
+	private LinkedList<ServerModel> activeQueue;
+	private ArrayList<ServerModel> fullServers;
+	
+	public ServerQueueModel(ArrayList<ServerModel> servers) {
+		// TODO fill in the constructor
+	}
+}
