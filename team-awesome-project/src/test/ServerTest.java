473f9bd465df544d7a3c5d3d626c4e4bfa35fcaa
mr-augustine <pushbuttonrobotics@gmail.com>
2014-11-12 21:47:08 -0600

    Added importTables() method to ServerTest

--- team-awesome-project/src/test/ServerTest.java
+++ team-awesome-project/src/test/ServerTest.java
@@ -1,22 +1,70 @@
 package test;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import javax.swing.*;
 
+import tabler.components.server.SectionModel;
+import tabler.components.server.ServerModel;
+import tabler.components.server.TableModel;
 import tabler.components.server.ServerQueueView;
 
 
 
+/**
+ * The ServerTest class exercises the ServerModel class which (in turn)
+ * exercises the SectionModel and TableModel classes
+ * 
+ * @author Kristin (krismndz)
+ * @author Augustine (mr-augustine)
+ *
+ */
+public class ServerTest {
+	private static String tablesFile = "./test/tables.txt";
+	private static String sectionsFile = "./test/sections.txt";
+	private static String serversFile = "./test/servers.txt";
+	
+	public static ArrayList<TableModel> importTables(String pathToTablesFile) {
+		if (pathToTablesFile.equals(null)) {
+			return null;
+		}
+		
+		Scanner inputFile = null;
+		
+		try {
+			inputFile = new Scanner(new File(tablesFile));
+		} catch (Exception FileNotFoundException) {
+			System.err.printf("Error: %s not found\n", inputFile);
+			System.exit(1);
+		}
+		
+		System.out.println("Contents of servers file\n");
+		
+		while (inputFile.hasNextLine()) {
+			System.out.print(inputFile.nextLine());
+		}
+		
+		return null; //new ArrayList<TableModel>();
+	}
+	
+	public static ArrayList<SectionModel> importSections(String pathToSectionsFile) {
+		return new ArrayList<SectionModel>();
+	}
 
-public class ServerTest extends JFrame{
+	public static ArrayList<ServerModel> importServers(String pathToServersFile) {
+		return new ArrayList<ServerModel>();
+	}
 	
 	public static void main (String[] args){
-
+		importTables(tablesFile);
 
 	
-		ServerQueueView window = new ServerQueueView();
+		/*ServerQueueView window = new ServerQueueView();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(700, 700);
-		window.setVisible(true);
+		window.setVisible(true);*/
 		
 	}
 }
