3011b1ee13f65d34fdc3055b9518e3e908556fc1
Roberto Mexquitic <rmstobi@gmail.com>
2014-11-13 13:49:25 -0600

    Updated GuestModel  I will add some more changes and validation checks.

--- team-awesome-project/src/tabler/components/guest/GuestModel.java
+++ team-awesome-project/src/tabler/components/guest/GuestModel.java
@@ -24,12 +24,25 @@ public class GuestModel {
 	 * Assuming that all variables are valid
 	 * Need to use ValidGuest Class before making Model
 	 */
-	public GuestModel(String name,String note, boolean rev, String contactNumber){
+public GuestModel(){
+		
+	}
+
+	public void createGuest(String name,String note, boolean rev, String contactNumber,int day,int month,int year,int hour,int minute) throws Exception{
+		if(checkName(name)==false){
+			throw new Exception("Enter Valid Name\n Must be one letter long");
+		}
+		if(!checkContact(contactNumber)){
+			throw new Exception("Enter Valid Contact Number\n Only (123) 456-7890");
+		}
+		
 		this.name=name;
 		this.note=note;
 		reservation=rev;
 		this.contactNumber=contactNumber;
 		created=new Date();
+		reservationTime=Calendar.getInstance();
+		reservationTime.set(year, month-1, day, hour, minute);
 		
 	}
 	
@@ -154,5 +167,101 @@ public class GuestModel {
 		}
 	}
 	
+	/*
+	 * Validation Methods
+	 * 
+	 */
+	
+	
+	/*
+	 * Use this method to check if the contact and name fields are both valid
+	 * Both must be valid to continue on.
+	 */
+	public static boolean checkGuest(String name,String contact){
+		if(checkContact(contact)&&checkName(name)){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	/*
+	 * will filter the contact string to leave only digits
+	 * Ideal input 1234567890 (123) 456 7890 (450)-897-4578
+	 * Only "." "()" "-" are allowed anything else will return false
+	 * Must be Ten digit number 
+	 */
+	private static boolean checkContact(String contact){
+		if(contact.length()<10){
+			return false;
+		}
+		else{
+			
+			String delims=" |\\(|\\)|\\-|\\.";
+			String[] holdString=contact.split(delims);
+			
+			if(checkNum(holdString)){
+					
+					
+			}
+			else{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/*
+	 * this method will check that the char in the line[] are all 
+	 * numberic numbers and that their are only and only 10 of them
+	 */
+	private static boolean checkNum(String line[]){
+		int count=0;
+
+		for(int i=0;i<line.length;i++){
+		
+			for(int j=0;j<line[i].length();j++){
+				if(!isInteger(String.valueOf(line[i].charAt(j)))){
+					return false;
+				}
+				count++;
+			}
+		}
+		if(count!=10){//if thier are not exactly ten then false
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/*
+	 * This will check if the char string is a number or not
+	 * if it is it will return true otherwise false
+	 */
+	private static boolean isInteger(String s) {
+	    try { 
+	        Integer.parseInt(s); 
+	    } catch(NumberFormatException e) { 
+	        return false; 
+	    }
+	    
+	    return true;
+	}
+	
+	
+	/*
+	 * this will check the name field 
+	 * as long as it is not empty it will be true
+	 */
+	private static boolean checkName(String name){
+		if(name.length()>0){
+			return true;
+		}
+		else{
+			return false;
+		}
+		
+	}
 	
 }
