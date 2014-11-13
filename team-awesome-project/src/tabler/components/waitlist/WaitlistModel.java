3a69ec62024e38899671893bfaff2d1a7bea26b0
mr-augustine <pushbuttonrobotics@gmail.com>
2014-11-13 10:50:26 -0600

    Created placeholder files for WaitlistModel and WaitlistTest

---
+++ team-awesome-project/src/tabler/components/waitlist/WaitlistModel.java
@@ -0,0 +1,7 @@
+package tabler.components.waitlist;
+
+public class WaitlistModel {
+	public WaitlistModel() {
+		System.out.println("The WaitlistModel constructor doesn't do anything yet");
+	}
+}
---
+++ team-awesome-project/src/test/WaitlistTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import tabler.components.waitlist.WaitlistModel;
+
+public class WaitlistTest {
+
+	public static void main(String[] args) {
+		WaitlistModel newWaitlist = new WaitlistModel();
+		
+		System.out.println(newWaitlist);
+	}
+
+}
