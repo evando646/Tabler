556da42a95c474a47986b9506186af28235c51c5
mr-augustine <pushbuttonrobotics@gmail.com>
2014-11-18 07:36:55 -0600

    Implemented basic importGuests() method in GuestTest; temporarily commented out main() code

--- team-awesome-project/src/test/GuestTest.java
+++ team-awesome-project/src/test/GuestTest.java
@@ -4,20 +4,61 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
+import java.io.File;
 import java.lang.Integer;
 
 import javax.swing.JFrame;
 //import Guest.ValidGuest;
 
+
+
+
+import tabler.components.guest.GuestModel;
+import tabler.components.guest.GuestView;
+
 public class GuestTest {
+	private static String guestsFile = "./src/test/guests.txt";
 	//private JFrame frame;
+
+	public static ArrayList<GuestModel> importGuests(String pathToGuestsFile) {
+		if (pathToGuestsFile == null) {
+			return null;
+		}
+		
+		Scanner inputFile = null;
+		ArrayList<GuestModel> importedGuests = new ArrayList<GuestModel>();
+		
+		try {
+			inputFile = new Scanner(new File(guestsFile));
+		} catch (Exception FileNotFoundException) {
+			System.err.printf("Error: %s not found\n", inputFile);
+			System.exit(1);
+		}
+		
+		// skip the field descriptor line
+		inputFile.nextLine();
+		
+		//System.out.println("Contents of guests file\n");
+		
+		while (inputFile.hasNextLine()) {
+			System.out.println(inputFile.nextLine());
+		}
+		
+		inputFile.close();
+		return importedGuests;
+	}
 
 	//@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
-
+		ArrayList<GuestModel> guests = null;
 		
+		guests = importGuests(guestsFile);
 		
+		for (GuestModel guest : guests) {
+			System.out.println(guest);
+		}
 		
+		/*
 		String name="Rob";
 		String contact="512-79-4857";
 		String note="Stuff and Things";
@@ -36,10 +77,9 @@ public class GuestTest {
 		window.registerListener(controller);
 		
 		window.frame.setVisible(true);
-
+		*/
 		
 		
 
 	}
-
 }
