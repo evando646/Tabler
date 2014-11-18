cfad3ee8e16381a81d07a5633c1fd85c5d29bb16
Manuel R Wong <manuelrwong@gmail.com>
2014-11-17 22:10:32 -0600

    -updated floor data once again to fix layout of tables on floor plan -updated floor view and floor test to display floor plan

--- team-awesome-project/src/tabler/components/floor/FloorView.java
+++ team-awesome-project/src/tabler/components/floor/FloorView.java
@@ -1,21 +1,39 @@
 package tabler.components.floor;
 
+import java.awt.Insets;
+import java.awt.Panel;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import tabler.components.table.TableModel;
 
 public class FloorView extends JFrame{
 	
+	private static final int SCALE = 5;
+	
+	private JPanel panel;
 	private ArrayList<JButton> tableButtons;
 	
 	public FloorView(ArrayList<TableModel> tableList)
 	{
+		panel = new JPanel();
 		
+		panel.setLayout(null);
 		
+		Insets insets = panel.getInsets();
 		
+		for( TableModel table : tableList )
+		{
+			JButton b = new JButton( "" + table.getTableNumber() );
+			panel.add(b);
+			b.setBounds(table.getPositionX() * SCALE + insets.left, table.getPositionY() * SCALE + insets.top, 
+					table.getWidth() * SCALE , table.getHeight() * SCALE);
+		}
+		
+		add(panel);
 	}
 	
 	
--- team-awesome-project/src/test/FloorTest.java
+++ team-awesome-project/src/test/FloorTest.java
@@ -17,7 +17,7 @@ public class FloorTest {
         view.register(controller);
 
         view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        view.setSize(400,300);
+        view.setSize(800,600);
         view.setVisible(true);
 
 	}
