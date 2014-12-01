bbfa3ccdc23f19f9a2dea0deec7ed7746a862841
krismndz <uqz585@my.utsa.edu>
2014-12-01 02:23:51 -0600

    updated ServerQueueController with comments and instructions for bolding chosen servers available tables in floorView

--- team-awesome-project/src/tabler/components/server/ServerQueueController.java
+++ team-awesome-project/src/tabler/components/server/ServerQueueController.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import javax.swing.JFrame;
 
+
 import tabler.components.table.*;
 import tabler.components.floor.*;
 import tabler.components.guest.*;
@@ -16,109 +17,159 @@ public class ServerQueueController implements ActionListener{
 
 	
 	public FloorModel model;
-
 	public FloorView view;
-
 	public ServerQueueModel queuemodel;
-
 	public ServerQueueView queueview;
-
 	public ArrayList<TableModel> tables;
-
 	public ArrayList<GuestModel> guests;
 
-	
+	/**
+	 * ServerQueueController constructor
+	 * @param model
+	 * @param view
+	 * @param queuemodel
+	 * @param guests
+	 * @param queueview
+	 */
 	public ServerQueueController( FloorModel model, FloorView view, ServerQueueModel queuemodel, ArrayList<GuestModel>guests,
 				ServerQueueView queueview){
 
 		this.model = model;
-
 		this.view = view;
-
-		this.queuemodel = queuemodel;
-	
-		//this.tables = model.getTableList();
-	
+		this.queuemodel = queuemodel;	
+		this.tables = model.getTableList();
 		this.guests= guests;
-	
 		this.queueview = queueview;
 
 	}
 
+	/**
+	 * actionPreformed takes in an event and evaluates to the correct change 
+	 * to be implemented
+	 * @param ActionEvent event
+	 */
 	@Override
-
 	public void actionPerformed(ActionEvent event) {
 		System.out.println("Pressed " + getActionString(event) + "\n");
 		String e = getActionString(event);
-		if (e.equals("choose") || e.equals("skip")
-				||e.equals("revert") || e.equals("previous")){
+		
+		//preliminary check for isEmpty() to avoid any NullPointerException errors
+		if(queuemodel.isEmpty()){
+			queueview.WaitError();
+			return;
+		}
+		
+		//Implement listAction if any one of these buttons are pressed
+		if (e.equals("view") || e.equals("skip")
+				||e.equals("hide") || e.equals("previous")){
 			listAction(e);
 			return;
 		}
 		
+		//if not sent to listAction, user is prompted to confirm decision to seat table
 		int confirm = queueview.showOptions("table " + e);
-		
 		if(confirm == 1){
-
 			return;
-	
 		}
+		
+		//size of total number of tables
 		int len = tables.size();
 	
 		for(int i = 0; i < len; i++){
-		
-			if( tables.get(i).getTableNumber() == Integer.parseInt(event.getActionCommand())){
+			
+			//find table that corresponds to tableButton pressed
+			if(tables.get(i).getTableNumber() == Integer.parseInt(event.getActionCommand())){
+				
+				//respond with error if table is already taken
 				if(tables.get(i).isOccupied() == true){
 					queueview.unavailableError(e);
 					return;
 				}
-				ServerQueueModel.updateQueue(tables.get(i));
-
+				
+				//otherwise update activeQueue using information extracted from selected tableModel
+				queuemodel.updateQueue(tables.get(i));
+				
+				//Temporary call for testing purposes
+				//Assign the guest to note table as OCCUPIED
 				tables.get(i).assignGuest(guests.get(i));
+		
+				//Sets all table button borders to empty
+				view.editBorders(tables, "hide");
+				
+				//once activeQueue is updated, update and show the queueView with the re-
+				//organized list data
+				queueview.editShownList(queuemodel);
+				
+				int index = queueview.getChosenIndex();
+				//Temporary test check 
+				if(queuemodel.isEmpty()){
+					queueview.WaitError();
+					return;
+				}
+				
+				/**view.editBorders(tables, "show");*/
+				 System.out.printf("Visible Index: %d", queueview.getChosenIndex());
 				}
 			
 			else{
 				continue;
 			}
-	
 		}
-
 	}
+	/**
+	 * getActionString() method returns the string of the action occured
+	 * @param event
+	 * @return String instance of the event- button pressed
+	 */
 	public String getActionString(ActionEvent event){
 		String e = event.getActionCommand();
 		return e;
 	}
-	
+	/**
+	 * listAction() method results in commanding changes for any 
+	 * direct ServerQueueView buttons being pressed. 
+	 * Creates bold outlines on specified serverModel's available tables
+	 * to be shown on the FloorView
+	 * @param String e : the actionEvent string
+	 */
 	public void listAction(String e){
 		
+		int index = queueview.getChosenIndex();
+		ArrayList<TableModel>tables = queuemodel.getList().get(index).getSection().availableTables();
 		if (e.equals("skip")){
+			view.editBorders(tables, "hide");
 			queueview.skip(queuemodel);
+			index = queueview.getChosenIndex();
+			tables.clear();
+			tables = queuemodel.getList().get(index).getSection().availableTables();
+			/**view.editBorders(tables, e);*/
+			System.out.printf("Visible Index: %d", index);
 			return;
 		} 
-		else if (e.equals("choose")){
-			int confirm = queueview.showOptions(e + " server (servername)");
-			if(confirm == 0){//means yes
-				int index = queueview.getChosenIndex();
-				/**String table = queueview.chooseTablePopUp(queuemodel.getList().get(index));*/
-				ServerQueueModel.dequeue(queuemodel.getList().get(index));
-				queueview.editDefaultList(queuemodel);
+		else if (e.equals("view")){
+				index = queueview.getChosenIndex();
+				tables = queuemodel.getList().get(index).getSection().availableTables();
+				view.editBorders(tables, e);
+				 System.out.printf("Visible Index: %d", index);
 				return;
-			}
-			else{
-				return;
-			}
-			
 		}
 		else if (e.equals("previous")){
+			view.editBorders(tables, "hide");
 			queueview.viewPrev(queuemodel);
+			index = queueview.getChosenIndex();
+			tables.clear();
+			tables = queuemodel.getList().get(index).getSection().availableTables();
+			 System.out.printf("Visible Index: %d", index);
+			/**view.editBorders(tables, e);*/
 			return;
 		}
-		else if (e.equals("revert")){
+		else if (e.equals("hide")){
+			view.editBorders(tables, e);
 			queueview.revert(queuemodel);
+			 System.out.printf("Visible Index: %d", index);
 			return;
 		}
 	}
 
 
 }
-
