abb3f2ee858603286f8484a491df78cd99276fb7
krismndz <uqz585@my.utsa.edu>
2014-12-01 02:42:04 -0600

    EditBorders method added to FloorView with comments. Responsive to ServerQueueController

--- team-awesome-project/src/tabler/components/floor/FloorView.java
+++ team-awesome-project/src/tabler/components/floor/FloorView.java
@@ -1,13 +1,20 @@
 package tabler.components.floor;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Insets;
 import java.awt.Panel;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
+import tabler.components.server.*;
 import tabler.components.table.TableModel;
 import tabler.components.server.*;
 public class FloorView extends JFrame{
@@ -19,6 +26,8 @@ public class FloorView extends JFrame{
 	
 	public FloorView(ArrayList<TableModel> tableList)
 	{
+		tableButtons = new ArrayList<JButton>();
+		
 		panel = new JPanel();
 		
 		panel.setLayout(null);
@@ -28,6 +37,7 @@ public class FloorView extends JFrame{
 		for( TableModel table : tableList )
 		{
 			JButton b = new JButton( "" + table.getTableNumber() );
+			tableButtons.add(b);
 			panel.add(b);
 			b.setBounds(table.getPositionX() * SCALE + insets.left, table.getPositionY() * SCALE + insets.top, 
 					table.getWidth() * SCALE , table.getHeight() * SCALE);
@@ -36,10 +46,57 @@ public class FloorView extends JFrame{
 		add(panel);
 	}
 	
-	
+	/**
+	 * editBorders() method results in attempted creation or removal 
+	 * of borders around a set of table buttons
+	 * @param ArrayList<TableModel> TablesList: desired tables to edit
+	 * @param String S: instruction
+	 * @author Kristin
+	 */
+	public void editBorders(ArrayList<TableModel> TablesList, String S){
+		Border border;
+		JPanel contentPane = (JPanel) this.getContentPane();
+		if (S.equals("hide")){
+			border =  BorderFactory.createEmptyBorder();
+		}
+		else{
+			border = new LineBorder(Color.black, 7);
+		}
+		int bsize = this.tableButtons.size();
+		int lsize = TablesList.size();
+		for(int i = 0; i < bsize; i ++){
+			for (int j = 0; j < lsize; j++){
+				if (this.tableButtons.get(i).getText().equals(Integer.toString(TablesList.get(j).getTableNumber()))){
+					tableButtons.get(i).setBorder(border);
+				}
+			}
+		}
+		contentPane.updateUI();
+	}
+	/**
+	 * Reinstated register method for floor buttons for testing
+	 * @param FloorController controller
+	 * @author Kristin
+	 */
+	public void register (FloorController controller)
+	{
+		for( JButton btn : tableButtons )
+		{
+
+			btn.addActionListener(controller);
+		}
+	}
+	/**
+	 * Reinstated register method of floor buttons for ServerQueueController
+	 * @param ServerQueueController controller
+	 */
 	public void register (ServerQueueController controller)
 	{
-		
+		for( JButton btn : tableButtons )
+		{
+			btn.addActionListener(controller);
+		}
 	}
+
 
 }
