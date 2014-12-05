adba798ad7021d86845dbced60d18d75ebd28564
mr-augustine <pushbuttonrobotics@gmail.com>
2014-12-04 18:26:30 -0600

    Added WaitlistView to the MainPanel

--- team-awesome-project/src/tabler/MainPanel.java
+++ team-awesome-project/src/tabler/MainPanel.java
@@ -6,7 +6,11 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Toolkit;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -28,6 +32,7 @@ import tabler.components.server.ServerQueueView;
 import tabler.components.waitlist.WaitlistModel;
 import tabler.components.waitlist.WaitlistView;
 import tabler.components.guest.GuestModel;
+import test.WaitlistTest;
 
 public class MainPanel extends JPanel {
 	
@@ -63,10 +68,14 @@ public class MainPanel extends JPanel {
         this.add(floorView,BorderLayout.WEST);
 
         //Waitlist
+        ArrayList<GuestModel> guests = importGuests("./src/test/guests.txt");
         WaitlistModel waitlistModel = new WaitlistModel();
+        for (GuestModel guest : guests) {
+        	waitlistModel.addGuest(guest);
+        }
         WaitlistView waitlistView = new WaitlistView(waitlistModel);
  
-        waitlistView.add( new JLabel("Waitlist") );
+        //waitlistView.add( new JLabel("Waitlist") );
 
         
         //ServerQueue
@@ -97,5 +106,97 @@ public class MainPanel extends JPanel {
                qview.createAndShowGui();
             }
          });
+	}
+
+	/**
+	 * Imports pre-determined guests from a guests file
+	 * 
+	 * @param pathToGuestsFile path to the guests file
+	 * @return a list of guests that were imported from the guests file
+	 */
+	public static ArrayList<GuestModel> importGuests(String pathToGuestsFile) {
+		if (pathToGuestsFile == null) {
+			return null;
+		}
+		
+		Scanner inputFile = null;
+		ArrayList<GuestModel> importedGuests = new ArrayList<GuestModel>();
+		//Enable this variable if you want to limit the number of guests to
+		//import. Also change the while condition and enable the variable
+		//decrement at the end of the while loop
+		//int numImportGuests = 9;
+		
+		try {
+			inputFile = new Scanner(new File(pathToGuestsFile));
+		} catch (Exception FileNotFoundException) {
+			System.err.printf("Error: %s not found\n", inputFile);
+			System.exit(1);
+		}
+		
+		// skip the field descriptor line
+		inputFile.nextLine();
+		
+		//System.out.println("Contents of guests file\n");
+		
+		GregorianCalendar now = new GregorianCalendar();
+		
+		// Use this while-condition to import some of the guests
+		//while (inputFile.hasNextLine() && numImportGuests > 0) {
+		// Use this while-condition to import all of the guests
+		while (inputFile.hasNextLine()) {
+			String nextGuest = inputFile.nextLine();
+			Scanner guestScanner = new Scanner(nextGuest);
+			guestScanner.useDelimiter(",");
+			
+			String name = guestScanner.next();
+			String contact = guestScanner.next();
+			int partySize = guestScanner.nextInt();
+			
+			int dayOffset = guestScanner.nextInt();
+			int hourOffset = guestScanner.nextInt();
+			int minuteOffset = guestScanner.nextInt();
+			int secondOffset = guestScanner.nextInt();
+			
+			GregorianCalendar created = (GregorianCalendar) now.clone();
+			created.add(Calendar.DAY_OF_MONTH, dayOffset);
+			created.add(Calendar.HOUR, hourOffset);
+			created.add(Calendar.MINUTE, minuteOffset);
+			created.add(Calendar.SECOND, secondOffset);
+			
+			String rDayOffset = guestScanner.next();
+			String rHourOffset = guestScanner.next();
+			String rMinuteOffset = guestScanner.next();
+			String rSecondOffset = guestScanner.next();
+	
+			GregorianCalendar reservationStart = (GregorianCalendar) now.clone();
+			reservationStart.add(Calendar.DAY_OF_MONTH, (rDayOffset.equals("-") ? dayOffset : Integer.parseInt(rDayOffset)));
+			reservationStart.add(Calendar.HOUR, (rHourOffset.equals("-") ? hourOffset : Integer.parseInt(rHourOffset)));
+			reservationStart.add(Calendar.MINUTE, (rMinuteOffset.equals("-") ? minuteOffset : Integer.parseInt(rMinuteOffset)));
+			reservationStart.add(Calendar.SECOND, (rSecondOffset.equals("-") ? secondOffset : Integer.parseInt(rSecondOffset)));
+			
+			String note;
+	
+			if (guestScanner.hasNext()) {
+				note = guestScanner.next();
+			} else {
+				note = "";
+			}
+			
+			GuestModel newGuest = null;
+			try {
+				newGuest = new GuestModel(name, note, contact, 
+					partySize, created, reservationStart);
+			} catch (Exception e) {
+				
+			}
+			
+			if (newGuest != null) { importedGuests.add(newGuest); }
+			//System.out.println(nextGuest);
+			guestScanner.close();
+			//numImportGuests--;
+		}
+		
+		inputFile.close();
+		return importedGuests;
 	}
 }
